61ac1cf94140f37de46da88c97d9142d6db9809e
Ritesh Kumar <riteshjha@Riteshs-MacBook-Air.local>
2023-06-02 00:17:42 +0530

    added dockerfile

--- main/config.py
+++ main/config.py
@@ -1,16 +1,16 @@
 
 paths = {
-        "data_dir"   : "/home/mycomputer/Documents/store_sales_data/",
+        "data_dir"   : "../data",
         "features"   : "Features data set.csv",
         "sales_data" : "sales data-set.csv",
         "store_data" : "stores data-set.csv",
 
-        "stage_dir"  : "/home/mycomputer/Documents/stage_data",
+        "stage_dir"  : "./stage_data",
         "train"      : "train_df.csv",
         "validation" : "validation.csv",
         "test"       : "test_df.csv",
 
-        "result_dir" : "/home/mycomputer/Documents/results",
+        "result_dir" : "./results",
         "prediction" : "prediction.csv",
         "feature_importance" : "feature_importance.csv"
         }
